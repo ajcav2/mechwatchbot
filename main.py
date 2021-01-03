761b9481dcdc79d081257b5a9b2b4b6de2065916
Alex <ajcav2@gmail.com>
2021-01-02 20:55:49 -0600

    Adding note to message me for tool improvements

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class RedditUser():
                                                         '`/gb <search_term>` : : watch for group buy  \n\n'+
                                                         '`/ic <search_term>` : : watch for interest check  \n\n'+
                                                         '`/rm <search_term>` : : remove search_term from watch list  \n\n'+
-                                                        '`/va` : : view all watch list')
+                                                        '`/va` : : view all watch list\n\nThis bot is still in beta! Message /u/ballsacagawea69 with any bugs and suggestions.')
 
 
     def alert_author(self, title, submission):

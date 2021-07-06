7fed907f7736f2068f45afd6ed9fe8ce557d3614
Alex <ajcav2@gmail.com>
2021-07-05 20:30:54 -0500

    another fix

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def send_watch_list(reddit_user):
     reddit.inbox.message(reddit_user.thread_id).reply(body)
 
 
-def send_alert(reddit_user, submission):
+def send_alert(reddit_user, submission, retries=5):
     tries_so_far = 0
     while tries_so_far < retries:
         try:
